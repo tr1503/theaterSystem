2ae17c69092b4786b9ee23cbd181cb2db3e535b8
tr1503 <hanlinye6@gmail.com>
2018-11-30 13:37:25 +0800

    Change Signup Page

--- user_signUp_Login/signUp.php
+++ user_signUp_Login/signUp.php
@@ -6,30 +6,63 @@ general terminal - sign up
     <head>
         <meta charset="UTF-8">
         <title>HYZtheater</title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     </head>
     <body>
-        <nav>
-            <div class="homepageLink">
-                <ul>
-                    <li><a href="home.php">HYZtheater</a></li>
-                </ul>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-nav-demo" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="home.php" class="navbar-brand">HYZtheater</a>
+                </div>
             </div>
         </nav>
-        <div class="userSignUp">
-            <h2>Sign Up</h2>
-            <form method="post" action="signUpProcess.php">
-                <div>
-                    Username: <input type="text" name="username" maxlength="15">*
-                </div>
-                <div>
-                    Password: <input type="text" name="password" maxlength="10">*
-                </div>
-                <div>
-                    Phone: <input type="text" name="phone" maxlength="10">
-                </div>
-                <input type="submit" value="Register">
-            </form>
-            <a href="login.php">Already have an account?</a>
-        </div>
+        <div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <div class="panel-title">Sign Up</div>
+                        <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="login.php" onclick="$('#signupbox').hide(); $('#loginbox').show()">Login</a></div>
+                    </div>  
+                    <div class="panel-body" >
+                        <form id="signupform" class="form-horizontal" role="form">
+                            <div id="signupalert" style="display:none" class="alert alert-danger">
+                                <p>Error:</p>
+                                    <span></span>
+                            </div>   
+                            <div class="form-group">
+                                <label for="username" class="col-md-3 control-label">Username</label>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" name="username" placeholder="Username">*
+                                </div>
+                            </div> 
+                            <div class="form-group">
+                                <label for="password" class="col-md-3 control-label">Password</label>
+                                <div class="col-md-9">
+                                    <input type="password" class="form-control" name="password" placeholder="Password">*
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone" class="col-md-3 control-label">Phone</label>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" name="phone" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group">                                    
+                                <div class="col-md-offset-3 col-md-9">
+                                    <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Register</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>      
+         </div> 
+        </div> 
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
     </body>
 </html>
